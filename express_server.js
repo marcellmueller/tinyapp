5ee258eb42a42d939c1063c19761383ce854d2a5
marcellmueller <mlmueller@protonmail.com>
2020-11-09 14:42:09 -0800

    fixed longURL variable in '/urls/:shortURL' route

--- express_server.js
+++ express_server.js
@@ -29,7 +29,7 @@ app.get('/urls', (req, res) => {
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
-    longURL: req.params.longURL,
+    longURL: urlDatabase[req.params.shortURL],
   };
   res.render('urls_show', templateVars);
 });
